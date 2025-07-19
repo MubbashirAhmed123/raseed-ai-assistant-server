11d87313c344280511a719b9c729634055cbac5d
Abdul Kareem <rajab8321@gmail.com>
2025-07-19 16:47:59 +0530

    Notification Function Added for web

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
-import vertexRoutes from "./testRoutes";
+// import vertexRoutes from "./testRoutes";
+import NotificationRoutes from "./notificationRoutes";
 
 export default async function mainRoutes(app: FastifyInstance) {
-  app.register(vertexRoutes, { prefix: '/api' });
+  // app.register(vertexRoutes, { prefix: '/api' });
+  app.register(NotificationRoutes, { prefix: "/api/v1/notification" });
 }
