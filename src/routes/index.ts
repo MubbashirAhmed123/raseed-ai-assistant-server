a12d96dd24540740d504db28f3a9577c6debfecd
roshan <roshan@neokred.tech>
2025-07-19 16:31:07 +0530

    added image extraction functionality

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from "fastify";
 import vertexRoutes from "./testRoutes";
+import extractRoutes from "./extractRoute";
 
 export default async function mainRoutes(app: FastifyInstance) {
   app.register(vertexRoutes, { prefix: '/api' });
+
+  app.register(extractRoutes, { prefix: '/api' });
+
 }
